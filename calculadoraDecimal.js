1a3522e063b91568442c81279e1c8e1ab164c416
Maicom M. Rodeghiero <maicom_mr@hotmail.com>
2018-03-28 20:29:17 -0300

    Atualização calculadora (WIP)

--- calculadoraDecimal.js
+++ calculadoraDecimal.js
@@ -20,14 +20,44 @@ function exibirResultadosDoDecimal() {
     }
     
     // INÍCIO INVERSOR DA STRING BINÁRIO
+    var binCru = ResultadoDecBin;
+
     ResultadoDecBin = ResultadoDecBin.split(""); //converte a variável para array;
     ResultadoDecBin = ResultadoDecBin.reverse(); //reverte os algarismos/dígios em ordem;
     ResultadoDecBin = ResultadoDecBin.join(""); //junta todos os valores novamente conforme a ordem inversa.
     // FIM INVERSOR DA STRING BINÁRIO
  
+
+    
+    var binNumeral = binCru.length;
+    var binOcatal = ResultadoDecBin;
+    binNumeral = binOcatal % 3;
+
+    while (binNumeral > 0) {
+        binOcatal = binOcatal + "0";
+        binNumeral = binOcatal % 3;
+    }
+
+    alert(binOcatal);
+    binOcatal = binOcatal.match(/.{1,3}/g);
+    alert(binOcatal);
+    var execucoes = binOcatal.length;
+    while (execucoes > 0){
+        execucoes
+        if  (binOcatal[execucoes] = "000"){
+            
+        }
+
+        execucoes = execucoes - 1;        
+    }
+
+
+
+
+
     document.getElementById("resultadoDecimalUm").setAttribute('value', ResultadoDecBin);
-    document.getElementById("resultadoDecimalDois").setAttribute('value', "abc");
-    document.getElementById("resultadoDecimalTres").setAttribute('value', "abc");
+    document.getElementById("resultadoDecimalDois").setAttribute('value', binNumeral);
+    document.getElementById("resultadoDecimalTres").setAttribute('value', binOcatal);
 
 
 }
